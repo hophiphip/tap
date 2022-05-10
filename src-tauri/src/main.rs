cfe74b0959558d07ab79e89981988a1787a2bc43
hophiphip <nighop@yandex.ru>
2022-05-10 22:08:25 +0400

    IPC tests

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,25 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+
+#[tauri::command]
+fn log_text() {
+  println!("log");
+}
+
 fn main() {
+  let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+  let close = CustomMenuItem::new("close".to_string(), "Close");
+  let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close));
+  let menu = Menu::new()
+    .add_native_item(MenuItem::Copy)
+    .add_item(CustomMenuItem::new("hide", "Hide"))
+    .add_submenu(submenu);
+
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![log_text])
+    .menu(menu)
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
+import { invoke } from '@tauri-apps/api/tauri';
+
 function App() {
+  const invoke = window.__TAURI_IPC__;
+
   const [count, setCount] = useState(0)
 
   return (
@@ -11,7 +15,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <button type="button" onClick={() => { setCount((count) => count + 1); invoke('log_text'); }}>
             count is: {count}
           </button>
         </p>
